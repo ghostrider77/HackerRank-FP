030891bdfaa3d9bd98df646c7e09bd892da9672d
Tamas Kurics <tamas.kurics@gmail.com>
2019-03-31 14:44:21 +0200

    add more problems to the adhoc folder
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ src/main/scala/adhoc/ListAndGCD.scala
@@ -0,0 +1,30 @@
+package adhoc
+
+object ListAndGCD {
+  type CanonicalForm = Map[Int, Int]
+
+  private def convertToIntList(line: String): List[Int] = line.split(" ").map(_.toInt).toList
+
+  private[adhoc] def readCanonicalForm(line: String): CanonicalForm =
+    convertToIntList(line).grouped(2).map{ case List(p, exp) => (p, exp) }.toMap
+
+  def calcGCD(numbers: List[CanonicalForm]): CanonicalForm = {
+    def mergeCanonicalForms(acc: CanonicalForm, n: CanonicalForm): CanonicalForm = {
+      for {
+        (p, exp) <- n
+        expInAcc: Int = acc.getOrElse(p, 0)
+        if expInAcc > 0
+      } yield p -> math.min(exp, expInAcc)
+    }
+
+    numbers.reduceLeft(mergeCanonicalForms)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val reader: Iterator[String] = scala.io.Source.stdin.getLines()
+    val n: Int = reader.next().toInt
+    val numbers: List[CanonicalForm] = (0 until n).map(_ => readCanonicalForm(reader.next())).toList
+    val result: CanonicalForm = calcGCD(numbers)
+    println(result.keys.toList.sorted.flatMap(prime => List(prime, result(prime))).mkString(" "))
+  }
+}
--- src/test/scala/adhoc/AdhocSuite.scala
+++ src/test/scala/adhoc/AdhocSuite.scala
@@ -16,14 +16,37 @@ class AdhocSuite extends FreeSpec with Matchers {
 
   "RotateString" - {
     import RotateString.getRotations
-    val strings = List("abc", "abcde", "abab", "aaa", "z")
-    strings.map(getRotations(_).toList) shouldEqual
-      List(
-        List("bca", "cab", "abc"),
-        List("bcdea", "cdeab", "deabc", "eabcd", "abcde"),
-        List("baba", "abab", "baba", "abab"),
-        List("aaa", "aaa", "aaa"),
-        List("z")
-      )
+
+    "should retrieve all rotations of each string in a list" in {
+      val strings = List("abc", "abcde", "abab", "aaa", "z")
+      strings.map(getRotations(_).toList) shouldEqual
+        List(
+          List("bca", "cab", "abc"),
+          List("bcdea", "cdeab", "deabc", "eabcd", "abcde"),
+          List("baba", "abab", "baba", "abab"),
+          List("aaa", "aaa", "aaa"),
+          List("z")
+        )
+    }
+  }
+
+  "ListAndGCD" - {
+    import ListAndGCD.{CanonicalForm, readCanonicalForm, calcGCD}
+
+    "should calculate the greatest common divisor of elements of a list given in their canonical forms" - {
+      "test case 1" in {
+        val n: Int = 2
+        val lines: List[String] = List("7 2", "2 2 7 1")
+        val numbers: List[CanonicalForm] = lines.map(readCanonicalForm)
+        calcGCD(numbers) shouldEqual Map(7 -> 1)
+      }
+
+      "test case 2" in {
+        val n: Int = 4
+        val lines: List[String] = List("2 2 3 2 5 3", "3 2 5 3 11 1", "2 2 3 3 5 4 7 6 19 18", "3 10 5 15")
+        val numbers: List[CanonicalForm] = lines.map(readCanonicalForm)
+        calcGCD(numbers) shouldEqual Map(3 -> 2, 5 -> 3)
+      }
+    }
   }
 }
